5742159a195a3dcab2f736abdba57de53241de9a
Sakshi <sakshibadyal09@gamil.com>
2025-03-03 10:29:52 +0530

    work

---
+++ nested.c
@@ -0,0 +1,156 @@
+/*   *****  
+     *****
+     *****
+           */
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=3;i++){    /*outer loop -> no of line*/
+//         for(int i=1;i<=n;i++){  /*inner no starts in each line */
+//             printf("*");
+            
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+/*   **** 
+     ****
+     ****
+           */
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=4;i++){
+//         for(int i=1;i<=n;i++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a numbr:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=4;i++){
+//         for(int i=1;i<=n;i++){
+//             printf("%d",i);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a row:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("* ");
+            
+//         }
+//         printf("\n");
+        
+//     }
+    
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n+1-i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a numbr:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n+1-i;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n+1-i;j++){
+//             printf("%d ",j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a number:");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         int a=1;
+//         for(int j=1;j<=n;j++){
+//             printf("%d ",a);
+//             a=a+2;
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*n-1;j=j+2){
+            printf("%d ",j);
+    
+        }
+        printf("\n");
+    }
+}
